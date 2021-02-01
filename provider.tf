85d3b427c0cdee1ea7c781c3b598191d667e6924
rameshkyadav82 <rameshkyadav82@gmail.com>
2021-02-01 12:43:26 +0800

    updated security access key file

--- provider.tf
+++ provider.tf
@@ -2,6 +2,5 @@
 # it as the default, and it can be referenced as `aws`.
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZI3VEFOA7HUS6WPL"
-  secret_key = "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"
+
 }
